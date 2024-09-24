d0838bf7b96ea70346f2f924de2dc17ecf4ddc0a
Davide Valsecchi <davide.valsecchi@cern.ch>
2024-09-24 08:43:37 +0200

    renamed folder

--- nanoaod_cfgs/RunIISummer20UL18/ttHbb_p1j_EFTcenter_5F_tbarqqtlnu_madspin/crabConfig.py
+++ nanoaod_cfgs/RunIISummer20UL18/ttHbb_p1j_EFTcenter_5F_tbarqqtlnu_madspin/crabConfig.py
@@ -8,14 +8,14 @@ config.General.transferLogs    = False
 
 config.JobType.pluginName  = 'PrivateMC'
 config.JobType.psetName    = 'nanoAOD_step_fake.py'
-config.JobType.inputFiles  = ['scriptExe.sh', 'genSim_step.py','digiRaw_noHLT_step.py', 'HLT_step.py','recoAOD_step.py','miniAOD_step.py','nanoAOD_step.py','pileup.py']
+config.JobType.inputFiles  = ['scriptExe.sh', 'genSim_step.py','digiRaw_noHLT_step.py', 'HLT_step.py','recoMiniAOD_step.py','nanoAOD_step.py','pileup.py']
 config.JobType.scriptExe   ='scriptExe.sh'
 config.JobType.numCores    = 2
 config.JobType.maxMemoryMB = 5000
 
 
 config.Data.splitting   = 'EventBased'
-config.Data.unitsPerJob = 2000
+config.Data.unitsPerJob = 7000
 NJOBS = 500
 config.Data.totalUnits  = config.Data.unitsPerJob * NJOBS
 config.Data.outLFNDirBase = '/store/user/dvalsecc/PrivateMC/'
